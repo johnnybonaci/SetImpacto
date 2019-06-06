9e434b2c5c7074dc8ddf9c1cf7d15182f3c8ad35
jbonaci <johnnybonaci@gmail.com>
2019-06-06 11:15:28 -0300

    orden de los parametros

--- class/Service.php
+++ class/Service.php
@@ -2,11 +2,11 @@
 
 class Service {
    
-    public function act_maestro_servicios($SolicitudImpactada = null, $ObjetoImpactado = null, $FechaImpacto = null, $DerivacionImpactada = null){
+    public function act_maestro_servicios($SolicitudImpactada = null, $ObjetoImpactado = null, $DerivacionImpactada = null,$FechaImpacto = null){
 			
-			if(empty($SolicitudImpactada) || empty($ObjetoImpactado) || empty($FechaImpacto) || empty($DerivacionImpactada)){
+		if(empty($SolicitudImpactada) || empty($ObjetoImpactado) || empty($FechaImpacto) || empty($DerivacionImpactada)){
 					return array('400', 'Bad Request');
-			}
+		}
  	
 
     	if(!preg_match('/^[a-z]+$/i', $SolicitudImpactada)){
@@ -17,13 +17,12 @@ class Service {
     		return array('400', 'Bad Request - Objeto Impactado invalido.');	
     	} 
 
+    	if(!preg_match('/^[a-z]+$/i', $DerivacionImpactada)){
+    		return array('400', 'Bad Request - Derivacion Impactada invalida.');	
+		} 
     	if(!preg_match('/^[0-9]{14}$/', $FechaImpacto)){
     		return array('400', 'Bad Request - Fecha de Impacto invalida.');
     	}
-
-    if(!preg_match('/^[a-z]+$/i', $DerivacionImpactada)){
-    		return array('400', 'Bad Request - Derivacion Impactada invalida.');	
-		} 
 		
 		
 		//$ksh = "/portal_judicial/app/movil/intervenciones/interfaces/ims_radio/servicio_instanlink/script/procesa_servicio_instanlink.ksh $SolicitudImpactada $ufmi $ObjetoImpactado $FechaImpacto";
